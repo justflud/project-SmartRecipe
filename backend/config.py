0b69f7777ef04c6af7eeccbeb0f2357f66ba9b29
Metilka <136602389+Metilka@users.noreply.github.com>
2026-03-18 17:19:16 +0400

    Revert "Update config.py"
    This reverts commit e421ad32a7fb5ee4b63a422c9391215b0501c898.

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:000@localhost:5432/med_diet_db'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
