e421ad32a7fb5ee4b63a422c9391215b0501c898
Metilka <136602389+Metilka@users.noreply.github.com>
2026-03-18 17:16:26 +0400

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:000@localhost:5432/med_diet_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
